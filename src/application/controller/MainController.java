0a0e18b9f7237f791da7f9c5a7e824fd647ec59a
Benito <obregonbenito@gmail.com>
2017-11-25 22:01:54 -0600

    added level select menu and basic level one.

--- src/application/controller/MainController.java
+++ src/application/controller/MainController.java
@@ -1,7 +1,35 @@
 package application.controller;
 
-public class MainController {
-	
-	//TODO Implement Event that leads to Level Selection View
+import application.Main;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class MainController implements EventHandler<ActionEvent>
+{
+	@Override
+	public void handle(ActionEvent event) 
+	{
+		// print a message to the console (debugging step, to ensure everything is connected)
+		System.out.println("\"Start\" button clicked!");
+		try 
+		{
+			// change over to a second view
+			// Load the FXML
+			Parent root = FXMLLoader.load(getClass().getResource("/LevelSelectView.fxml"));
+			
+			// Add the scene to the stage
+			Main.stage.setScene(new Scene(root, 800, 500));
+			
+			// Show the stage to the user
+			Main.stage.show();
+		}
+		catch(Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
 
 }
